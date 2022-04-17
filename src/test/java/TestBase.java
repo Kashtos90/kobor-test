eb8c9694a1a55bf5f39eb72582c999220142cae2
Sergei Kashtuev <29061990a@gmail.com>
2022-04-18 00:31:18 +0700

    added fixes

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Selenide.clearBrowserLocalStorage;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
 import static io.restassured.RestAssured.sessionId;
 import static org.openqa.selenium.devtools.v85.network.Network.clearBrowserCookies;
 
@@ -21,7 +21,8 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        Configuration.baseUrl = "https://plarium.com/ru/";
+        baseUrl = "https://plarium.com/ru/";
+        open(baseUrl);
         DriverConfig.configure();
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
