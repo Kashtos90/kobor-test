72902d992bbef99665e7124cdd232b997f9fb943
Sergei Kashtuev <29061990a@gmail.com>
2022-04-18 00:37:16 +0700

    added fixes

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -8,13 +8,11 @@ import org.aeonbits.owner.Config;
 })
 public interface WebDriverConfig extends Config {
 
-
     String browser();
     String browserVersion();
     String browserSize();
     String browserMobileView();
     String remoteDriverUrl();
     String videoStorage();
-    String timeout();
 }
 
