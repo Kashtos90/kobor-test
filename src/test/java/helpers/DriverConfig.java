beec801ce2a0dcc037c4a6bebfb487f862546f8c
Sergei Kashtuev <29061990a@gmail.com>
2022-04-17 23:45:36 +0700

    added fixes

--- src/test/java/helpers/DriverConfig.java
+++ src/test/java/helpers/DriverConfig.java
@@ -14,6 +14,7 @@ public class DriverConfig {
         Configuration.browser = Project.webConfig.browserName();
         Configuration.browserVersion = Project.webConfig.browserVersion();
         Configuration.browserSize = Project.webConfig.browserSize();
+        Configuration.remote = Project.webConfig.remoteUrl();
         Configuration.timeout = Project.webConfig.timeout();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
