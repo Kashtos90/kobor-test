575ddeb4c92d13c2c696571d18c37621b441bcb9
Sergei Kashtuev <29061990a@gmail.com>
2022-04-17 21:42:52 +0700

    added fixes in TestBase.java

--- src/test/java/config/TestBase.java
+++ src/test/java/config/TestBase.java
@@ -5,9 +5,11 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.AllureAttach;
 import helpers.Attach;
 import helpers.DriverConfig;
+import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.clearBrowserLocalStorage;
@@ -15,6 +17,7 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static io.restassured.RestAssured.sessionId;
 import static org.openqa.selenium.devtools.v85.network.Network.clearBrowserCookies;
 
+@ExtendWith({AllureJunit5.class})
 public class TestBase {
 
     @BeforeAll
@@ -23,8 +26,6 @@ public class TestBase {
 
         DriverConfig.configure();
         Configuration.baseUrl = "https://plarium.com/ru/";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
