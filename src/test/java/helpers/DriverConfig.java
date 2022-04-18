2098c9d8ab0706b5f20fd0fc99bc2d1345b3aa33
Sergei Kashtuev <29061990a@gmail.com>
2022-04-18 22:42:46 +0700

    added fixes

--- src/test/java/helpers/DriverConfig.java
+++ src/test/java/helpers/DriverConfig.java
@@ -27,7 +27,7 @@ public class DriverConfig {
                 chromeOptions.addArguments("--disable-infobars");
                 chromeOptions.addArguments("--disable-popup-blocking");
                 chromeOptions.addArguments("--disable-notifications");
-                chromeOptions.addArguments("--lang=en-en");
+                chromeOptions.addArguments("--lang=ru-ru");
                 capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
                 break;
             case "firefox":
