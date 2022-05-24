46e433393c4115866eea985d2102b7fda4acd5ad
Sergei Kashtuev <29061990a@gmail.com>
2022-05-25 00:35:59 +0700

    added fix

--- src/test/java/PlariumTests.java
+++ src/test/java/PlariumTests.java
@@ -1,7 +1,5 @@
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Cookie;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -9,9 +7,7 @@ import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
-import static io.restassured.RestAssured.given;
 
 public class PlariumTests extends TestBase {
 
